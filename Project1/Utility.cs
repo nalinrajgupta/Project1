2258bffda71ac8838de4eb157885290bff16a90a
Nalin Gupta <nagup@microsoft.com>
2015-11-03 09:03:11 +0530

    Initial commit with Utility.Print method.

---
+++ Project1/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public static class Utility
+    {
+        public static void Print(string message)
+        {
+            Console.WriteLine("Printing inside Project1.Utility, message : {0} ", message);
+        }
+    }
+}
